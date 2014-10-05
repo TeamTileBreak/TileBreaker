6feee7129deb246b52ce5477ff305217fef45ad0
Logan Swango <lbswango@email.uark.edu>
2014-10-05 18:46:43 -0500

    Fixed upgardeactivity after merge

--- app/src/main/java/edu/uark/csce/tilebreaker/UpgradeActivity.java
+++ app/src/main/java/edu/uark/csce/tilebreaker/UpgradeActivity.java
@@ -13,15 +13,6 @@ public class UpgradeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        View decorView = getWindow().getDecorView();
-// Hide the status bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-// Remember that you should never show the action bar if the
-// status bar is hidden, so hide that too if necessary.
-        ActionBar actionBar = getActionBar();
-        actionBar.hide();
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upgrade);
