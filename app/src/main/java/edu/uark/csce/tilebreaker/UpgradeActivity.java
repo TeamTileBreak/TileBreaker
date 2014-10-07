00cd181c23f0c22a8f839cc6cf35e63bb455db27
Logan Swango <lbswango@email.uark.edu>
2014-10-07 09:08:55 -0500

    Needs Testing of first upgrade drag

--- app/src/main/java/edu/uark/csce/tilebreaker/UpgradeActivity.java
+++ app/src/main/java/edu/uark/csce/tilebreaker/UpgradeActivity.java
@@ -1,12 +1,20 @@
 package edu.uark.csce.tilebreaker;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnDragListener;
+import android.widget.RelativeLayout;
 
 
 public class UpgradeActivity extends Activity {
@@ -35,6 +43,73 @@ public class UpgradeActivity extends Activity {
                 Intent intent = new Intent(UpgradeActivity.this, PauseActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        // ug1 drag listener
+        final ImageButton twoBall = (ImageButton) findViewById(R.id.ug1);
+        twoBall.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
+
+                String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+                ClipData dragData = new ClipData("2 ball", mimeTypes, item);
+
+                // Instantiates the drag shadow builder.
+                View.DragShadowBuilder myShadow = new DragShadowBuilder(twoBall);
+
+                // Starts the drag
+                v.startDrag(dragData,  // the data to be dragged
+                        myShadow,  // the drag shadow builder
+                        null,      // no need to use local data
+                        0          // flags (not currently used, set to 0)
+                );
+                return true;
+            }
+        });
+
+        // Create and set the drag event listener for the View
+        twoBall.setOnDragListener( new OnDragListener(){
+            @Override
+            public boolean onDrag(View v,  DragEvent event){
+                switch(event.getAction())
+                {
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        //layoutParams = (RelativeLayout.LayoutParams)
+                                //v.getLayoutParams();
+                        Log.d("Hello, ", "Action is DragEvent.ACTION_DRAG_STARTED");
+                        // Do nothing
+                        break;
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        Log.d("Hello, ", "Action is DragEvent.ACTION_DRAG_ENTERED");
+                        int x_cord = (int) event.getX();
+                        int y_cord = (int) event.getY();
+                        break;
+                    case DragEvent.ACTION_DRAG_EXITED :
+                        Log.d("Hello, ", "Action is DragEvent.ACTION_DRAG_EXITED");
+                        x_cord = (int) event.getX();
+                        y_cord = (int) event.getY();
+                       // layoutParams.leftMargin = x_cord;
+                       // layoutParams.topMargin = y_cord;
+                       // v.setLayoutParams(layoutParams);
+                        break;
+                    case DragEvent.ACTION_DRAG_LOCATION  :
+                        Log.d("Hello, ", "Action is DragEvent.ACTION_DRAG_LOCATION");
+                        x_cord = (int) event.getX();
+                        y_cord = (int) event.getY();
+                        break;
+                    case DragEvent.ACTION_DRAG_ENDED   :
+                        Log.d("Hello, ", "Action is DragEvent.ACTION_DRAG_ENDED");
+                        // Do nothing
+                        break;
+                    case DragEvent.ACTION_DROP:
+                        Log.d("Hello, ", "ACTION_DROP event");
+                        // Do nothing
+                        break;
+                    default: break;
+                }
+                return true;
             }
         });
     }
